b9f805a0b40012adf324ee51ea264e7787844911
ccik2285 <ccik1653@gmail.com>
2024-12-27 02:45:11 +0900

    chore: 쓰지 않는 코드 삭제

--- src/main/java/io/hhplus/tdd/infrastructure/JpaLectureRepository.java
+++ src/main/java/io/hhplus/tdd/infrastructure/JpaLectureRepository.java
@@ -3,7 +3,8 @@ package io.hhplus.tdd.infrastructure;
 import io.hhplus.tdd.entity.Lecture;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface JpaLectureRepository extends JpaRepository<Lecture,Long>{
-    Lecture findById();
 
 }
--- src/main/java/io/hhplus/tdd/infrastructure/JpaStudentRepository.java
+++ src/main/java/io/hhplus/tdd/infrastructure/JpaStudentRepository.java
@@ -4,5 +4,5 @@ import io.hhplus.tdd.entity.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface JpaStudentRepository extends JpaRepository<Student,Long> {
-    public Student findById();
+
 }
